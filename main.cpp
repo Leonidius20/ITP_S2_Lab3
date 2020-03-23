251f64e9e22ee6a8bbf8d3802b0a40d65e06fad2
Leonidius20 <leonidslupsky2002@gmail.com>
2020-03-23 20:57:29 +0200

    implemented main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include "shunting_yard.h"
+#include "token.h"
+#include "calculator.h"
 
 using namespace std;
 
-int main() {
-    // TODO
+int main(int argc, char *argv[]) {
+    if (argc == 0) {
+        cerr << "Arithmetic expression is not specified" << endl;
+        return 0;
+    }
+
+    string expression;
+    for (int i = 0; i < argc; i++) {
+        expression.append(argv[i]);
+    }
+
+    vector<Token> tokens = parse(expression);
+    double result = compute(tokens);
+    cout << result << endl;
 }
