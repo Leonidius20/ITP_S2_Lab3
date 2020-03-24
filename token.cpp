57b2823ef8b76ec9e2673b7b3df4b1fd8510c260
Oleksandr Bezushko <34242332+Insiderser@users.noreply.github.com>
2020-03-24 11:27:59 +0200

    Implement token functionality (#2)

--- token.cpp
+++ token.cpp
@@ -1,1 +1,21 @@
 #include "token.h"
+#include <cmath>
+#include <stdexcept>
+
+using namespace std;
+
+double Operator::apply(double a, double b) {
+    if (signature == "+") {
+        return a + b;
+    } else if (signature == "-") {
+        return a - b;
+    } else if (signature == "*") {
+        return a * b;
+    } else if (signature == "/") {
+        return a / b;
+    } else if (signature == "^") {
+        return pow(a, b);
+    } else {
+        throw invalid_argument("Unknown operator: " + signature);
+    }
+}
